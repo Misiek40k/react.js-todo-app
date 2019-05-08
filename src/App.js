e880e84ac6020c1034894f9a04904d62bd8cbddc
Misiek40k <comanderrr@gmail.com>
2019-05-08 22:57:53 +0200

    mdb edge header added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { MDBFreeBird, MDBCol, MDBRow, MDBCardBody, MDBCardTitle, MDBContainer, MDBEdgeHeader } from
+  "mdbreact";
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App grey lighten-2">
+      <MDBContainer className="mw-100 mh-100 p-0">
+        <MDBEdgeHeader color="mdb-color deep-purple darken-2"></MDBEdgeHeader>
+        <MDBFreeBird>
+          <MDBRow className="p-3">
+            <MDBCol md="8" lg="7" className="mx-auto float-none white z-depth-2 rounded">
+              <MDBCardBody>
+                <MDBCardTitle>React.js TODO App</MDBCardTitle>
+                <p className="pb-4">Add, Remove, Clear</p>              
+              </MDBCardBody>
+            </MDBCol>
+          </MDBRow>
+        </MDBFreeBird>
+      </MDBContainer>
     </div>
   );
 }
