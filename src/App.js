525aec0b7f7907b2435a0397f9ff1add246d9881
Misiek40k <comanderrr@gmail.com>
2019-05-09 19:55:58 +0200

    title state added

--- src/App.js
+++ src/App.js
@@ -6,26 +6,41 @@ import './App.scss';
 
 import FooterComponent from './components/footer';
 
-function App() {
-  return (
-    <div className="App grey lighten-4 min-vh-100">
-      <MDBContainer className="mw-100 mh-100 p-0">
-        <MDBEdgeHeader color="mdb-color deep-purple"></MDBEdgeHeader>
-        <MDBFreeBird>
-          <MDBRow className="p-3">
-            <MDBCol md="8" lg="7" className="mx-auto float-none white z-depth-2 rounded">
-              <MDBCardBody>
-                <MDBCardTitle>React.js TODO App</MDBCardTitle>
-                <p className="pb-4">Add, Remove, Clear</p>              
-              </MDBCardBody>
-            </MDBCol>
-          </MDBRow>
-        </MDBFreeBird>
-      </MDBContainer>
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      title: "React.js TODO App"
+    }
+  }
+  
+  render() {
+    return (
+      <div className="App grey lighten-4 min-vh-100">
+        <MDBContainer className="mw-100 mh-100 p-0">
+          {/* edge header */}
+          <MDBEdgeHeader color="deep-purple"></MDBEdgeHeader>
 
-      <FooterComponent/>
-    </div>
-  );
+          {/* Main container */}
+          <MDBFreeBird>
+            <MDBRow className="p-3">
+              <MDBCol md="8" lg="7" className="mx-auto float-none white z-depth-2 rounded">
+                <MDBCardBody>
+
+                  <MDBCardTitle>{this.state.title}</MDBCardTitle>
+                  <p className="pb-4">Add, Remove, Clear</p>
+
+                </MDBCardBody>
+              </MDBCol>
+            </MDBRow>
+          </MDBFreeBird>
+
+          {/* footer */}
+          <FooterComponent />
+        </MDBContainer>
+      </div>
+    );
+  }
 }
 
 export default App;
