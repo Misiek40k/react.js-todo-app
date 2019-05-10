4f7b2bab31e2b1b1325c7917ad684fdd5310eaa0
Misiek40k <comanderrr@gmail.com>
2019-05-10 17:23:07 +0200

    btn group flex direction

--- src/components/btnGroup.js
+++ src/components/btnGroup.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import { MDBBtn, MDBBtnGroup} from "mdbreact";
 
+import '../styles/btnGroup.scss';
+
 class BtnGroupComponent extends Component {
     render() {
         return (
             <div>
-                <MDBBtnGroup className="mb-4">
-                    <MDBBtn color="primary">Add</MDBBtn>
-                    <MDBBtn color="warning">Remove last</MDBBtn>
+                <MDBBtnGroup className="mb-4 btnGroup">
+                    <MDBBtn color="primary btn">Add</MDBBtn>
+                    <MDBBtn color="warning">Delete last</MDBBtn>
                     <MDBBtn color="danger">Clear</MDBBtn>
                 </MDBBtnGroup>
             </div>
