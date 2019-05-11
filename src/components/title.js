b9496665c987fb93eee02851b8ad5a41cac13b31
Misiek40k <comanderrr@gmail.com>
2019-05-11 16:23:48 +0200

    logo added

--- src/components/title.js
+++ src/components/title.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { MDBCardTitle } from 'mdbreact';
+import { MDBCardTitle, MDBRow, MDBCol } from 'mdbreact';
+
+import logo from '../assets/img/logo.png';
 
 
 const TitleComponent = () => {
     return (
-        <div>
+        <div className="">
+            <MDBRow className="mb-4 justify-content-center">
+                <MDBCol size="5">
+                    <img src={logo} className="img-fluid" alt="logo" />
+                </MDBCol>
+            </MDBRow>
             <MDBCardTitle>React.js TODO App</MDBCardTitle>
             <p className="pb-4">Add task / Delete last / Clear all</p>
         </div>
